f5d6845d59e8a3f26cd37908f6efc900e4dee703
Bill Robertson <billrobertson42@gmail.com>
2017-03-11 22:19:40 -0500

    Finishing touches on script

--- make.sh
+++ make.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+version=`date +%Y%m%dt%H%M%S`
+echo "Version ${version}"
 lein clean
 rm -rf exploded
 lein ring uberwar
 mkdir exploded
 cd exploded
 unzip ../target/*.war
-cd ..
-echo "Run dev_appserver.sh exploded to start the server in dev mode"
-echo "Run appcfg.sh update exploded to push the changes to GAE."
+cd WEB-INF
+perl -p -i -e "s/<version>1/<version>${version}/" appengine-web.xml
+cd ../..
+echo 'Run "dev_appserver.sh exploded" to start the server in dev mode'
+echo 'Run "appcfg.sh update exploded" to push the changes to GAE.'
