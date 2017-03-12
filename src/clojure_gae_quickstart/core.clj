e8d2cd6263155a820cb150435fbc396a76b2fc66
Bill Robertson <billrobertson42@gmail.com>
2017-03-11 20:27:51 -0500

    Add welcome feature and minor refactor

--- src/clojure_gae_quickstart/core.clj
+++ src/clojure_gae_quickstart/core.clj
@@ -1,19 +1,31 @@
 (ns clojure-gae-quickstart.core
   (:require [cheshire.core :as json]
             [compojure.core :refer :all]
+            [compojure.route :refer [not-found]]
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.util.response :as ring]))
 
-(defn hello-response[request]
-  (->
-   {:body (json/generate-string {:name "\u4E16\u754C"})}
-   (ring/content-type "application/json")
-   (ring/charset "UTF-8")))   
+(defn wrap-welcome [handler welcome]
+  (fn [request]
+    (let [response (handler request)]
+      (if (and (or (nil? response) (= 404 (:status response)))
+               (.endsWith (:uri request) "/"))
+        (handler (assoc request :uri (str (:uri request) welcome)))
+        response))))
+
+(defn json-response [body]
+  (-> (ring/response (json/generate-string body))
+      (ring/content-type "application/json")
+      (ring/charset "UTF-8")))
+
+(defn demo[request]
+  (json-response {:name "\u4E16\u754C"}))
 
 (defroutes app-routes
-  (GET "/demo" [] hello-response)
-  (GET "/" [] (ring/redirect "/index.html")))
+  (GET "/demo" [] demo)
+  (not-found ""))
 
 (def app
   (-> app-routes
-      (wrap-resource "public")))
+      (wrap-resource "public")
+      (wrap-welcome "index.html")))
